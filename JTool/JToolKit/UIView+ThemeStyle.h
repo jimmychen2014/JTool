929b44da6a1d562941b4f2273f859d34b7dad65a
Jingting Chen <jingting@douban.com>
2019-08-20 17:52:53 +0800

    JToolKit: Add UIView ThemeStyle category.

---
+++ JTool/JToolKit/UIView+ThemeStyle.h
@@ -0,0 +1,17 @@
+//
+//  UIView+ThemeStyle.h
+//  JTool
+//
+//  Created by Jingting Chen on 2019/8/20.
+//  Copyright © 2019 Jingting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (ThemeStyle)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JTool/JToolKit/UIView+ThemeStyle.m
@@ -0,0 +1,13 @@
+//
+//  UIView+ThemeStyle.m
+//  JTool
+//
+//  Created by Jingting Chen on 2019/8/20.
+//  Copyright © 2019 Jingting. All rights reserved.
+//
+
+#import "UIView+ThemeStyle.h"
+
+@implementation UIView (ThemeStyle)
+
+@end
